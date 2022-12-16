45b5ea7fadd69383a4f1b3eba61c51b99490604e
legendary0911 <85240150+legendary0911@users.noreply.github.com>
2022-12-16 13:51:22 +0530

    completed

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -9,14 +9,14 @@ function ProjectCard(props) {
 
         <div className="">
             <div data-aos="zoom-in-up" data-aos-duration="600" class=" max-w-sm shadow-lg hover:shadow-xl shadow-black/70 hover:shadow-black/80 hover:scale-150 duration-200 mx-4 my-4 bg-[#111827] rounded-lg border border-gray-200 shadow-md  dark:border-gray-700">
-                <a href="#">
+                <a >
                     <img class="rounded-t-lg mx-auto w-[100%]" src={props.img} alt=""></img>
                 </a>
                 <div class="p-5">
-                    <a href="#">
+                    <a >
                         <h5 class="mb-2 text-2xl font-bold tracking-tight text-white">{props.name}</h5>
                     </a>
-                    <p class="mb-3 font-normal text-gray-400">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+                    <p class="mb-8 font-normal text-gray-400">{props.desc}</p>
                     <a href={props.sitelink} class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         Link
                         <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
